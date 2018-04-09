549f5273219af88ecf9e56669b1e4e4ab83daa5a
李子翔 <zixiangli@ctrip.com>
2018-04-09 19:47:56 +0800

    update

--- src/common/loading/Loading.js
+++ src/common/loading/Loading.js
@@ -2,14 +2,17 @@ import React from "react"
 import loadingImg from "./loading.gif"
 import "./loading.styl"
 
-class Loading extends React.Component {
+class Loading extends React.PureComponent {
   render() {
-    const displayStyle = this.props.show ? { display: "" } : { display: "none" };
+    const { show = false, title = '正在载入...' } = this.props;
+    if (!show) {
+      return null
+    }
     return (
-      <div className="loading-container" style={displayStyle}>
+      <div className="loading-container">
         <div className="loading-wrapper">
           <img src={loadingImg} width="18px" height="18px" alt="loading" />
-          <div className="loading-title">{this.props.title}</div>
+          <div className="loading-title">{title}</div>
         </div>
       </div>
     );
