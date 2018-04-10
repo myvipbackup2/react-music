71e5c71975d6323f60db8c8edfca869dcbd6236f
李子翔 <zixiangli@ctrip.com>
2018-04-10 22:35:34 +0800

    add redux-dev-tool

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,19 +1,27 @@
-import React from "react"
-import "./header.styl"
+import React from 'react'
+import './header.styl'
 
 class MusicHeader extends React.Component {
 
   handleBack = () => {
-    window.history.back();
+    const { history, location } = window;
+    if (history.length > 2) {
+      window.history.back();
+    } else {
+      const { pathname } = location;
+      const arr = pathname.split('/');
+      arr.pop();
+      window.location.href = `${arr.join('/')}`
+    }
   };
 
   render() {
     return (
-      <div className="music-header">
-				<span className="header-back" onClick={this.handleBack}>
-					<i className="icon-back" />
+      <div className='music-header'>
+				<span className='header-back' onClick={this.handleBack}>
+					<i className='icon-back' />
 				</span>
-        <div className="header-title">
+        <div className='header-title'>
           {this.props.title}
         </div>
       </div>
