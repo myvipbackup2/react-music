4643698776b124f17b40df088205beab8b80a458
李子翔 <zixiangli@ctrip.com>
2018-04-10 22:35:01 +0800

    add redux-dev-tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux"
+import { createStore } from 'redux'
 import reducer from "./reducers"
 
+// 生产环境是https，且域名没有端口号 不开启redux-dev-tool
+const { protocol, host } = window.location;
+const isDev = protocol === 'http:' && host.includes(':');
+
 // 创建store
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  isDev && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export default store
