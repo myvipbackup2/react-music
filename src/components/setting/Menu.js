f85577f9be7f596ca4b8ea91b0792fff81638797
李子翔 <zixiangli@ctrip.com>
2018-04-14 00:04:28 +0800

    update

--- src/components/setting/Menu.js
+++ src/components/setting/Menu.js
@@ -1,54 +1,64 @@
 import React from "react"
-import {CSSTransition} from "react-transition-group"
+import { CSSTransition } from "react-transition-group"
 import Skin from "../../containers/Skin"
 
 import "./menu.styl"
 
 class Menu extends React.Component {
-	constructor(props) {
-		super(props);
 
-		this.state = {
-			skinShow: false
-		};
-	}
-	showSetting = (status) => {
-		this.close();
-        // menu关闭后打开设置
-        setTimeout(() => {
-        	this.setState({
-        		skinShow: status
-        	});
-        }, 300);
-	}
-	close = () => {
-        this.props.closeMenu();
-    }
-	render() {
-		return (
-			<div>
-				<CSSTransition in={this.props.show} timeout={300} classNames="fade"
-					onEnter={() => {
-						this.refs.bottom.style.display = "block";
-					}}
-					onExited={() => {
-						this.refs.bottom.style.display = "none";
-					}}>
-			      <div className="bottom-container" onClick={this.close}  ref="bottom">
-			        <div className="bottom-wrapper">
-			          <div className="item" onClick={() => {this.showSetting(true);}}>
-			            皮肤中心
-			          </div>
-			          <div className="item-close" onClick={this.close}>
-			            关闭
-			          </div>
-			        </div>
-			      </div>
-			    </CSSTransition>
-			    <Skin show={this.state.skinShow} close={() => {this.showSetting(false);}} />
-		  	</div>
-		);
-	}
+  state = {
+    skinShow: false
+  };
+
+  button = null;
+
+  showSetting = (status) => {
+    this.handleClose();
+    // menu关闭后打开设置
+    setTimeout(() => {
+      this.setState({
+        skinShow: status
+      });
+    }, 300);
+  };
+
+  handleClose = () => {
+    this.props.closeMenu();
+  };
+
+  getBottomRef = ref => {
+    this.button = ref;
+  };
+
+  render() {
+    return (
+      <div>
+        <CSSTransition
+          in={this.props.show}
+          timeout={300}
+          classNames="fade"
+          onEnter={() => {
+            this.bottom.style.display = "block";
+          }}
+          onExited={() => {
+            this.bottom.style.display = "none";
+          }}
+        >
+          <div className="bottom-container" onClick={this.close} ref={this.getBottomRef}>
+            <div className="bottom-wrapper">
+              <div className="item" onClick={() => {this.showSetting(true)}}>
+                皮肤中心
+              </div>
+              <div className="item-close" onClick={this.handleClose}>
+                关闭
+              </div>
+            </div>
+          </div>
+        </CSSTransition>
+        <Skin show={this.state.skinShow} close={() => {this.showSetting(false)}} />
+      </div>
+    );
+  }
 }
 
 export default Menu
